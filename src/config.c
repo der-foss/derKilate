53c2248e82e83e49e59504ac102a5199b8ebe05e
trindadedev13 <trindadedev13@gmail.com>
2026-03-08 03:22:50 -0300

    feat: Remove support for running multiple files in a single command
    Signed-off-by: trindadedev13 <trindadedev13@gmail.com>

--- src/config.c
+++ src/config.c
@@ -4,24 +4,17 @@
 #include "kilate/config.h"
 #include "kilate/vector.h"
 
-vector_t *files = NULL;
 vector_t *libs_directories = NULL;
 vector_t *libs_native_directories = NULL;
 
 void config_init()
 {
-        files = vector_make(sizeof(char *));
         libs_directories = vector_make(sizeof(char *));
         libs_native_directories = vector_make(sizeof(char *));
 }
 
 void config_end()
 {
-        for (size_t i = 0; i < files->size; ++i) {
-                char *filename = *(char **)vector_get(files, i);
-                free(filename);
-        }
-
         for (size_t i = 0; i < libs_directories->size; ++i) {
                 char *lib = *(char **)vector_get(libs_directories, i);
                 free(lib);
@@ -34,5 +27,4 @@ void config_end()
 
         vector_delete(libs_directories);
         vector_delete(libs_native_directories);
-        vector_delete(files);
 }
